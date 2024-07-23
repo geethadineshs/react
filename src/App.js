482c4ebfa37efa568e680f195bca360ff8153aa2
geethadineshs <162040133+geethadineshs@users.noreply.github.com>
2024-07-23 11:00:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Devops team.......</br> welcome all...........</p>
+        <p>Devops team.......<br /> welcome all...........</p>
       </header>
     </div>
   );
